9a0d388418ee572a48d80ce1e161da226c7e08a0
davefogo <davefogo@gmail.com>
2015-01-21 12:41:37 -0500

    built nav and added welcome controller, set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'welcome/features'
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/login'
+
   resources :invoices
 
-  root "invoices#index"
+  root to: "welcome#index"
+
 end
